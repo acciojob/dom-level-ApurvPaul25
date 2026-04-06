f84d83e5e657594e469abb8f284daee4f32b16c2
Apurva Paul___Kayden <47327058+ApurvPaul25@users.noreply.github.com>
2026-04-07 00:46:23 +0530

    level checked

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+let ele = document.getElementById('level')
+let cnt = 0
+
+while(ele){
+  cnt++
+  ele = ele.parentElement
+}
+
+alert("The level of the element is: " + cnt) 
